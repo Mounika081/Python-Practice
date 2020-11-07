6d7a278bb14d03d7eaef5849f575ad2e14ac1078
Mounika081 <mounikakorra2003@gmail.com>
2020-11-07 14:37:49 +0530

    first commit

---
+++ day2(15-09-2020)/sample.py
@@ -0,0 +1,22 @@
+print("hello all")
+print("welcome")
+print("hello world")
+print("sum=",44+77)
+
+n1=80
+n2=30
+print('before swaping:',n1,n2)
+n3=n1
+n1=n2
+n2=n3
+print('after swaping:',n1,n2)
+
+n1=80
+n2=30
+print('before swaping:',n1,n2)
+n1=n1+n2
+n2=n1-n2
+n1=n1-n2
+print('after swaping:',n1,n2)
+
+
---
+++ day2(15-09-2020)/sample2.py
@@ -0,0 +1,5 @@
+val=50
+a=int(input("Enter a value:"))
+b=int(input("Enter a value:"))
+print(a,type(a))
+print(a+b)
